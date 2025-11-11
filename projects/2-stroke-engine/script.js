2b0abb56fa5f3167f4ebf6f11ecbafce5720b3ab
Vincenzo Misuraca <misuracavincenzo10@gmail.com>
2025-11-11 01:18:22 +0100

    I edited the main page and the page with my info by updating cv lp and pt

--- projects/2-stroke-engine/script.js
+++ projects/2-stroke-engine/script.js
@@ -1,31 +1,34 @@
 const carouselItems = [
   {
-    // esempio pratico carosello foto
-    src: "img/2-stroke-engine-card.png",
-    alt: "Close-up render of the engine's intake manifold.",
-    title: "Close-up render of the engine's intake manifold",
-    description: "Example description 1",
+    //Example carousel
+    src: "img/carousel-1.jpeg",
+    alt: "Side View Render of the 2-Stroke Engine Block and Clutch.",
+    title: "Side View Render of the 2-Stroke Engine Block and Clutch",
+    description: "A view highlighting the clutch cover and its fastening bolts",
     element: null,
   },
   {
-    src: "https://lh3.googleusercontent.com/aida-public/AB6AXuCvUn488PzY6TZ1EjJMpM4Kh-EeOvPtMT9MlSJC-yCyf2wSS9fxkZTZEjwsaWr2Cfdkp7eT_bw3iWezsn8Sgfyo156eEI91EqVsTGo2wwOqpEQe0pzwMs-cYHbzhfNQ_nU4J_SPkJ56tBMA8XFT6UfJdfV2YR0t26IPkKPlV2X1ha1IZ-3harw-FBn9mmTw6osRVT6ks8_6hbTkwUK6_MTd7OxQSNxDtqvKJE49D4HHGpb13ynFFB0n7Ax1oeIkvuc1S3ML5f6J20M",
-    alt: "Detailed view of the engine valve cover with branding.",
-    title: "Detailed view of the engine valve cover with branding",
-    description: "Example description 2",
+    src: "img/carousel-2.jpeg",
+    alt: "Close-Up of the Stator Cover.",
+    title: "Close-Up of the Stator Cover",
+    description:
+      "A detailed view of the blue left side crankcase cover (stator side) made of Ergal",
     element: null,
   },
   {
-    src: "https://lh3.googleusercontent.com/aida-public/AB6AXuDm7_T9-6yDITQvd7qAGyPuy7j90wBpi_rSOlsEDiSHKIsELAwgQbaeICnWE0xkoI9x0hR7syVdqIuOMqAgn278GVIOb4V5_D8wgVRMJgpKzaVGyfN-Y_UWtOteFsGvPpxA_9nxRL71W5HNcIPOhrNZ7UVaCF4BbP-RWVkmnohEslpdYkUOxdKJL2ijPN90Pe6Z7mqcDpYVcu23mD5IAZG4CyKubXCXZhq70e0K4yNQXbE17ayOcJk5CEnKZ4NbOH-Oi-VmjFawpZE",
-    alt: "Render showing the turbocharger assembly and exhaust system.",
-    title: "Render showing the turbocharger assembly and exhaust system",
-    description: "Example description 3",
+    src: "img/carousel-3.jpeg",
+    alt: "Frontal Perspective Highlighting the Intake Manifold Mount.",
+    title: "Frontal Perspective Highlighting the Intake Manifold Mount",
+    description:
+      "Symmetrical frontal view showing the intake manifold area at the center of the engine block.",
     element: null,
   },
   {
-    src: "https://lh3.googleusercontent.com/aida-public/AB6AXuC6c3QM9pOgRzeX7b90Z4SziKgsPhApxFpfJskSalZFo4e2dGVChQGkFTYfzjICD0daPPmaGG7WQudLuUUHvpVReu1Jrq0qBQqwsNI-wZO-sBaIjZ2hFeSvPmEJfd7o_juAIElZRn06VO-Y42SkfR9o9hFqWpSqTv-UFBgrbOJZbq8dd1lOFxJw-GpiBHAJLN7L2aYNA3Hv6MuNP4quGXx3wp1F4kjGrMcf4NolMwIAPAlu8lVmM7cc0qlNvowyerwKW7DOYX9DL98",
-    alt: "DExploded view of the V8 engine components",
-    title: "Exploded view of the V8 engine components",
-    description: "Example description 4",
+    src: "img/carousel-4.jpeg",
+    alt: "Three-quarter Perspective view Render of the Engine Assembly",
+    title: "Three-quarter Perspective view Render of the Engine Assembly",
+    description:
+      "A dynamic perspective that highlights the general shape of the engine/gearbox casing and the output shaft",
     element: null,
   },
 ];
@@ -35,15 +38,22 @@ let carouselTitleHighlighted;
 let carouselDescriptionHighlighted;
 window.onload = () => {
   carouselImgHighlighted = document.getElementById("carousel-img-highlighted");
-  carouselTitleHighlighted = document.getElementById("carousel-title-highlighted");
-  carouselDescriptionHighlighted = document.getElementById("carousel-description-highlighted");
+  carouselTitleHighlighted = document.getElementById(
+    "carousel-title-highlighted"
+  );
+  carouselDescriptionHighlighted = document.getElementById(
+    "carousel-description-highlighted"
+  );
 
   const carouselOptions = document.getElementById("carousel-options");
   carouselItems.forEach((item, index) => {
     const container = document.createElement("div");
-    container.classList = "relative group cursor-pointer rounded-md overflow-hidden ring-2";
-    container.onclick = () => { changeCarouselContent(index); }
-    
+    container.classList =
+      "relative group cursor-pointer rounded-md overflow-hidden ring-2";
+    container.onclick = () => {
+      changeCarouselContent(index);
+    };
+
     const img = document.createElement("img");
     img.src = item.src;
     img.alt = item.alt;
@@ -51,7 +61,8 @@ window.onload = () => {
     container.appendChild(img);
 
     const randomDiv = document.createElement("div");
-    randomDiv.classList = "absolute inset-0 bg-black/30 group-hover:bg-black/10 transition-colors";
+    randomDiv.classList =
+      "absolute inset-0 bg-black/30 group-hover:bg-black/10 transition-colors";
     container.appendChild(randomDiv);
 
     carouselOptions.appendChild(container);
@@ -81,9 +92,9 @@ const prevCarouselItem = () => {
   let index = currentIndex - 1;
   if (index < 0) index = carouselItems.length - 1;
   changeCarouselContent(index);
-}
+};
 
 const nextCarouselItem = () => {
   const index = (currentIndex + 1) % carouselItems.length;
   changeCarouselContent(index);
-}
+};
